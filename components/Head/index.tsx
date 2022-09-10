b9f4c6d11e33f2deff697189eca1dd39296bbe67
Slothzieee <silviosperanza97@gmail.com>
2022-09-10 19:24:07 +0200

    verify google

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -10,6 +10,10 @@ const Head = ({ title }: Props) => {
 			<title>{title}</title>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			<meta name="description" content="A new fun way to learn" />
+			<meta
+				name="google-site-verification"
+				content="IpEsJilWyGT2yvDSr_pcq95F5KGVuzkUcmPEAYdqmCk"
+			/>
 			<meta name="title" content={title} />
 		</NextHead>
 	);
