1b2c576f81cfcc5de7dba9df9ccbbf2d0565af4b
Slothzieee <silviosperanza97@gmail.com>
2022-09-10 19:15:30 +0200

    p

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-console.log(process.env.GOOGLE_ID);
-
 export const authOptions = {
+	secret: process.env.SECRET,
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_ID || '',
